09b1cd7292c513c7d3ff6aee019876a9c46c4093
osman corluk <corluk@hotmail.com>
2022-08-07 17:43:11 +0300

    pre-switch redux-saga

--- src/features/todo/store/slice.ts
+++ src/features/todo/store/slice.ts
@@ -8,37 +8,105 @@ interface  Todo  {
 }
 interface  TodoState {
     todos : Todo[]
-    selected : Todo | null 
+    selected : Todo | null ,
+    errors : Error[] 
+    status : "idle" | "loading" | "failed"
 }
 const initialState : TodoState= {
 
     todos : [],
-    selected : null 
-    
+    selected : null, 
+    errors :  [],
+    status  : "idle"
 }
-let  baseURL : string  | undefined= "/"
-if(process.env.NODE_ENV == "development"){
+interface Event {
+    Key : string ,
+    Value : object, 
+    Timestamp : Date
+}
+const TodoSlice = (baseURL : string )=>{
 
-    baseURL = process.env.BASEURL 
-}
+
+    const Producer = createAsyncThunk("todos/producer" , async (event:Event)=>{
+
+
+        const uri = baseURL + "/events/todo" 
+        const response = await  fetch(uri,{
+            headers : {
+                "Content-Type" : "application/json",
+                "X_KEY" : event.Key,
+                "X_TIMESTAMP" : event.Timestamp.toISOString()
+            },
+            body : JSON.stringify(event.Value)
+        })
+        if (! response.ok){
+            throw new Error("response error:" + response.statusText)
+        }
+
+
+    })
 const asyncAdd = createAsyncThunk("todos/asyncAdd",async (todo:Todo)=>{
 
-    return (dispatch:Dispatch) =>{
+        
         const uri = baseURL + "/api/todos" 
-        fetch(uri , {
-            method : "POST"
-        } ).then(resp =>{
-            dispatch()
-        })
-    }
+        const response =  await fetch(uri , {
+            method : "POST",
+            headers : {
+                "Content-Type": "application/json"
+            
+            },
+            body : JSON.stringify(todo)
+
+        } )
+        return await response.json()
+ 
 })
-createSlice({
+
+const deleteAsync = createAsyncThunk("todos/delete" , async (id: number,thunkApi)=>{
+    
+    const event : Event = {
+        Key : "app.todo.delete", 
+        Value :{id} ,
+        Timestamp: new Date() 
+
+    }
+    await thunkApi.dispatch(Producer)
+})
+
+
+ 
+const Slice = createSlice({
     name: "todos", 
     initialState ,
     reducers: {
         addTodo : (state,action : PayloadAction<Todo>)=>{
             state.todos = [...state.todos , action.payload]
+        },
+        deleteTodo : (state , action : PayloadAction<number>)=>{
+
+            const index = state.todos.findIndex
         }
-        deleteTodo : (st)
+    },
+    extraReducers : (builder)=>{
+        builder
+        .addCase(Producer.pending,(state,action)=>{
+            state.status = "loading" 
+            
+        })
+    
+        .addCase(Producer.rejected, (state, action)=>{
+            state.status = "failed"
+            state.errors = [...state.errors , action.payload as Error ]
+           
+        })
+
+        .addCase(Producer.fulfilled,(state , action )=>{
+            state.status = "idle" 
+            
+        })
+        
     }
 })
+
+
+}
